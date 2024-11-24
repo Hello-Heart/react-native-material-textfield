5b7d3864fcf3666265ea117a69846e91bce8c5da
Maxim Toyberman <maxtoyberman@gmail.com>
2024-11-24 10:49:16 +0200

    Update styles.js

--- src/components/label/styles.js
+++ src/components/label/styles.js
@@ -3,15 +3,12 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     position: 'absolute',
-    top: 0,
-    left: '-100%',
-    width: '200%',
-    paddingLeft: '50%',
+    top: 0
   },
-
   text: {
     textAlign: 'left',
     includeFontPadding: false,
     textAlignVertical: 'top',
   },
 });
+
